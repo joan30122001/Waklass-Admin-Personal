ab987ea52e84552d3d9f60fa50b4271ad9d25fab
joan30122001 <ngounouloic675@gmail.com>
2024-07-29 19:17:29 +0100

    ajout d'une page pour la diffusion d'informations générales

--- assets/js/app.js
+++ assets/js/app.js
@@ -77,6 +77,53 @@ $(function() {
 
 	// switcher 
 
+	// $("#LightTheme").on("click", function() {
+	// 	$("html").attr("class", "light-theme")
+	// }),
+
+	// $("#DarkTheme").on("click", function() {
+	// 	$("html").attr("class", "dark-theme")
+	// }),
+
+	// $("#SemiDarkTheme").on("click", function() {
+	// 	$("html").attr("class", "semi-dark")
+	// }),
+
+	// $("#MinimalTheme").on("click", function() {
+	// 	$("html").attr("class", "minimal-theme")
+	// })
+
+
+	// $("#headercolor1").on("click", function() {
+	// 	$("html").addClass("color-header headercolor1"), $("html").removeClass("headercolor2 headercolor3 headercolor4 headercolor5 headercolor6 headercolor7 headercolor8")
+	// }), $("#headercolor2").on("click", function() {
+	// 	$("html").addClass("color-header headercolor2"), $("html").removeClass("headercolor1 headercolor3 headercolor4 headercolor5 headercolor6 headercolor7 headercolor8")
+	// }), $("#headercolor3").on("click", function() {
+	// 	$("html").addClass("color-header headercolor3"), $("html").removeClass("headercolor1 headercolor2 headercolor4 headercolor5 headercolor6 headercolor7 headercolor8")
+	// }), $("#headercolor4").on("click", function() {
+	// 	$("html").addClass("color-header headercolor4"), $("html").removeClass("headercolor1 headercolor2 headercolor3 headercolor5 headercolor6 headercolor7 headercolor8")
+	// }), $("#headercolor5").on("click", function() {
+	// 	$("html").addClass("color-header headercolor5"), $("html").removeClass("headercolor1 headercolor2 headercolor4 headercolor3 headercolor6 headercolor7 headercolor8")
+	// }), $("#headercolor6").on("click", function() {
+	// 	$("html").addClass("color-header headercolor6"), $("html").removeClass("headercolor1 headercolor2 headercolor4 headercolor5 headercolor3 headercolor7 headercolor8")
+	// }), $("#headercolor7").on("click", function() {
+	// 	$("html").addClass("color-header headercolor7"), $("html").removeClass("headercolor1 headercolor2 headercolor4 headercolor5 headercolor6 headercolor3 headercolor8")
+	// }), $("#headercolor8").on("click", function() {
+	// 	$("html").addClass("color-header headercolor8"), $("html").removeClass("headercolor1 headercolor2 headercolor4 headercolor5 headercolor6 headercolor7 headercolor3")
+	// })
+
+	$(".dark-mode").on("click", function() {
+
+		if($(".dark-mode-icon i").attr("class") == 'bi bi-brightness-high-fill') {
+			$(".dark-mode-icon i").attr("class", "bi bi-moon-fill");
+			$("html").attr("class", "")
+		} else {
+			$(".dark-mode-icon i").attr("class", "bi bi-brightness-high-fill");
+			$("html").attr("class", "dark-theme")
+		}
+
+	}), 
+
 	$("#LightTheme").on("click", function() {
 		$("html").attr("class", "light-theme")
 	}),
